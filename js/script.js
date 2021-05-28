9a657b1b3e1aef2e2f7120f06cab444be0df72e0
Ahmed Elsayed <57777850+AhmedTitef@users.noreply.github.com>
2021-05-28 00:00:43 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Software Engineer."],
+    strings: ["Software Engineer.", "Software Developer."],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
